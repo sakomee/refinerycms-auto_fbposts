4aca9b727b3e221ef713551e50a896386ad319a9
sakomee <somee12@hotmail.com>
2013-01-16 22:44:20 +0200

    modify views file path

--- app/models/observers/blog_post_observer.rb
+++
@@ -1,8 +0,0 @@
-class BlogPostObserver < ActiveRecord::Observer
-	observe Refinery::Blog::Post
-	def after_save(blog_post)
-		if (blog_post.post_facebook && blog_post.live?)  
-			blog_post.post_facebook!
-		end
-	end
-end

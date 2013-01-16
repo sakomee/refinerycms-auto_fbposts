f27b8c5ec3276c86ca4912fcd6aa6fbef66a4d22
sakomee <somee12@hotmail.com>
2013-01-16 21:01:54 +0200

    Add view file

--- lib/generators/refinery/auto_fbposts_generator.rb
+++ lib/generators/refinery/auto_fbposts_generator.rb
@@ -1,6 +1,6 @@
 module Refinery
   class AutoFbpostsGenerator < Rails::Generators::Base
-
+    
     def rake_db
       rake("refinery_auto_fbposts:install:migrations")
     end
@@ -14,6 +14,20 @@ module Refinery
         Refinery::AutoFbposts::Engine.load_seed
         EOH
       end
+    end
+    
+   source_root File.expand_path('../../../../', __FILE__)
+    
+    def Copy_files
+      copy_file 'app/views/admin/blog/posts/_form.html.erb'  
+      copy_file 'app/models/observers/blog_post_observer.rb'    
+      # copy_file File.expand_path('../../../../app/views/admin/blog/posts/_form.html.erb', __FILE__)      
+      # copy_file File.expand_path('../../../../app/models/observers/blog_post_observer.rb', __FILE__)  
+    end
+
+    def create_initializer_file
+      puts "Updating active_record.observers in application.rb"
+      application "config.active_record.observers = :blog_post_observer"
     end
   end
 end
